3c64d70ba4d22ae6a777ddd847189caf347c3048
Zhenya240315 <105649502+Zhenya240315@users.noreply.github.com>
2022-11-18 20:27:28 +0200

    hw-07-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector(".gallery");
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+const galleryImg = createGallaryMarkup(galleryItems);
+galleryItemsContainer.insertAdjacentHTML("beforeend", galleryImg);
+galleryItemsContainer.addEventListener("click", onImgClick);
+
+function onImgClick(evt) {
+  blockStandartAction(evt);
+  // если не IMG возвр return
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  // В ином случае открывае библиотеку basicLightbox
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  galleryItemsContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+// блокирует стандартные функц.браузера(не открывает картинку по ссылке)
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+    )
+    .join("");
+}
+
+const galleryImg = createGallaryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+galleryContainer.addEventListener("click", onImgClick);
+
+function onImgClick(evt) {
+  blockStandartAction(evt);
+}
+// блокирует стандартные функц.браузера(не открывает картинку по ссылке)
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+
+const galleryEl = new SimpleLightbox("ul.gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
